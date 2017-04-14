8c3830cde25d0ae32ed961928ef4cfd699772fca
João Gabriel Machado <joaogabriel013@gmail.com>
2017-04-14 12:23:14 -0300

    Criação da Classe Cliente

---
+++ src/Client/Cliente.java
@@ -0,0 +1,197 @@
+/*
+ * Código Criado por João Gabriel e Herber Medeiros
+ * 
+ * 
+ * Qualquer cópia deve conter o local de onde foi retirado o código
+ * 
+ * 
+ * Baseado em: http://www.devmedia.com.br/como-criar-um-chat-multithread-com-socket-em-java/33639
+ */
+
+package Client;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.Socket;
+import javax.swing.*;
+
+public class Cliente extends JFrame implements ActionListener, KeyListener {
+	// A classe herda de JFrame, possibilitando a criação de formulários e
+	// implementação das interfaces ActionListener e KeyListener para prover
+	// ações nos botões e ações das teclas, respectivamente.
+	private static final long serialVersionUID = 1L;
+	private JTextArea texto;
+	private JTextField txtMsg;
+	private JButton btnSend;
+	private JButton btnSair;
+	private JLabel lblHistorico;
+	private JLabel lblMsg;
+	private JPanel pnlContent;
+	private Socket socket;
+	private OutputStream ou;
+	private Writer ouw;
+	private BufferedWriter bfw;
+	private JTextField txtIP;
+	private JTextField txtPorta;
+	private JTextField txtNome;
+
+	public Cliente() throws IOException {
+		JLabel lblMessage = new JLabel("Verificar!");
+		txtIP = new JTextField("127.0.0.1");
+		txtPorta = new JTextField("12345");
+		txtNome = new JTextField("Cliente");
+		Object[] texts = { lblMessage, txtIP, txtPorta, txtNome };
+		JOptionPane.showMessageDialog(null, texts);
+		pnlContent = new JPanel();
+		texto = new JTextArea(10, 20);
+		texto.setEditable(false);
+		texto.setBackground(new Color(240, 240, 240));
+		txtMsg = new JTextField(20);
+		lblHistorico = new JLabel("Histórico");
+		lblMsg = new JLabel("Mensagem");
+		btnSend = new JButton("Enviar");
+		btnSend.setToolTipText("Enviar Mensagem");
+		btnSair = new JButton("Sair");
+		btnSair.setToolTipText("Sair do Chat");
+		btnSend.addActionListener(this);
+		btnSair.addActionListener(this);
+		btnSend.addKeyListener(this);
+		txtMsg.addKeyListener(this);
+		JScrollPane scroll = new JScrollPane(texto);
+		texto.setLineWrap(true);
+		pnlContent.add(lblHistorico);
+		pnlContent.add(scroll);
+		pnlContent.add(lblMsg);
+		pnlContent.add(txtMsg);
+		pnlContent.add(btnSair);
+		pnlContent.add(btnSend);
+		pnlContent.setBackground(Color.LIGHT_GRAY);
+		texto.setBorder(BorderFactory.createEtchedBorder(Color.BLUE, Color.BLUE));
+		txtMsg.setBorder(BorderFactory.createEtchedBorder(Color.BLUE, Color.BLUE));
+		setTitle(txtNome.getText());
+		setContentPane(pnlContent);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		setSize(250, 300);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+	public void conectar() throws IOException {// Método usado para conectar no
+												// server socket, retorna IO
+												// Exception caso dê algum erro.
+
+		socket = new Socket(txtIP.getText(), Integer.parseInt(txtPorta.getText()));
+		ou = socket.getOutputStream();
+		ouw = new OutputStreamWriter(ou);
+		bfw = new BufferedWriter(ouw);
+		bfw.write(txtNome.getText() + "\r\n");
+		bfw.flush();
+	}
+
+	public void enviarMensagem(String msg) throws IOException {// Método usado
+																// para enviar a
+																// mensagem
+
+		if (msg.equals("Sair")) {
+			bfw.write("Desconectado \r\n");
+			texto.append("Desconectado \r\n");
+		} else {
+			bfw.write(msg + "\r\n");
+			texto.append(txtNome.getText() + " diz -> " + txtMsg.getText() + "\r\n");
+		}
+		bfw.flush();
+		txtMsg.setText("");
+	}
+
+	public void escutar() throws IOException {
+		// Método usado para escutar (receber) mensagens do servidor. Toda vez
+		// que alguém enviar uma, o método será processado pelo servidor e envia
+		// para todos os clientes conectados, por isso a necessidade do código.
+
+		InputStream in = socket.getInputStream();
+		InputStreamReader inr = new InputStreamReader(in);
+		BufferedReader bfr = new BufferedReader(inr);
+		String msg = "";
+
+		while (!"Sair".equalsIgnoreCase(msg)) {
+			if (bfr.ready()) {
+				msg = bfr.readLine();
+				if (msg.equals("Sair"))
+					texto.append("Servidor caiu! \r\n");
+				else
+					texto.append(msg + "\r\n");
+			}
+		}
+	}
+
+	public void sair() throws IOException {// Usado para sair da conexão
+
+		enviarMensagem("Sair");
+		bfw.close();
+		ouw.close();
+		ou.close();
+		socket.close();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// Método usado para receber as ações dos botões dos usuários. Nele foi
+		// feito um chaveamento: se o usuário pressionar o botão “send” então
+		// será enviada uma mensagem, senão será encerrado o chat.
+
+		try {
+			if (e.getActionCommand().equals(btnSend.getActionCommand()))
+				enviarMensagem(txtMsg.getText());
+			else if (e.getActionCommand().equals(btnSair.getActionCommand()))
+				sair();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {// Acionado quando o usuário pressiona
+										// “Enter”, verificando se o key code é
+										// o Enter. Caso seja, a mensagem é
+										// enviada para o servidor.
+
+		if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+			try {
+				enviarMensagem(txtMsg.getText());
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		Cliente app = new Cliente();
+		app.conectar();
+		app.escutar();
+	}
+}
