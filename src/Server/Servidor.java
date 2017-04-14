5dd5fb5bc4d05579dee181cd2c9cc903595d130b
João Gabriel Machado <joaogabriel013@gmail.com>
2017-04-14 11:55:39 -0300

    Criação do Main
    Crianção do método Main na Classe Servidor

--- src/Server/Servidor.java
+++ src/Server/Servidor.java
@@ -86,4 +86,32 @@ public class Servidor extends Thread {
 			}
 		}
 	}
-}
+
+	public static void main(String[] args) {// Fará a configuração do servidor
+											// Socket e sua respectiva porta
+
+		try {
+			// Cria os objetos necessário para instânciar o servidor
+			JLabel lblMessage = new JLabel("Porta do Servidor:");
+			JTextField txtPorta = new JTextField("12345");
+			Object[] texts = { lblMessage, txtPorta };// Informa a porta do
+														// servidor
+			JOptionPane.showMessageDialog(null, texts);
+			server = new ServerSocket(Integer.parseInt(txtPorta.getText()));
+			clientes = new ArrayList<BufferedWriter>();
+			JOptionPane.showMessageDialog(null, "Servidor ativo na porta: " + txtPorta.getText());
+
+			while (true) {
+				System.out.println("Aguardando conexão...");
+				Socket con = server.accept();// Fica aguardando uma nova conexão
+				System.out.println("Cliente conectado...");
+				Thread t = new Servidor(con);
+				t.start();
+			}
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+	}// Fim do método main
+} // Fim da classe
